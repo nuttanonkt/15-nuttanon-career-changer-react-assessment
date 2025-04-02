9c2fbada9dd9f32292717f8f1cbb96e4fd81fb5d
nuttanon.kt <nuttanon321@gmail.com>
2025-04-02 21:46:49 +0700

    feat(create): 100% Home,owner 90%Admin 20%user

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,25 @@
-import { Outlet,Link } from 'react-router-dom'
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-    <nav className="bg-amber-800 p-4 border-b-1">
-      <ul className="flex justify-end items-center">
-        <li className="space-x-4 font-bold">
-          <Link to ="/" className="hover:text-gray-400">Home</Link>
-          <Link to = "/owner" className="hover:text-gray-400">Owner</Link>
-        </li>
-      </ul>
-    </nav>
-    <div>
-        <Outlet/>
+      <nav className="p-4 border-b-1">
+        <ul className="flex justify-end items-center">
+          <li className="space-x-4 font-bold">
+            <Link to="/" className="hover:text-gray-400">
+              Home
+            </Link>
+            <Link to="/owner" className="hover:text-gray-400">
+              Owner
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const NavigateAdmin = () => {
 }
 
   return (
-    <div className="bg-amber-400 text-center m-4">  
+    <div className=" text-center m-4">  
       <h1 className=" text-4xl font-bold ">Generation Thailand <br /> React - Assessment</h1> 
         <div className="mt-4"> 
           <button onClick ={NavaigateUser} className="bg-gray-300 py-2 px-4 rounded-lg hover:bg-gray-400 mx-2 my-2">
